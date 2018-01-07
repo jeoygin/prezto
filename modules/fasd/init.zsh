e31395c74ca3f31da586f5a35881628990400c1f
Jeoygin Wang <jeoygin@gmail.com>
2018-01-07 14:32:07 -0800

    Add fcd function and it's tab completion

--- modules/fasd/init.zsh
+++ modules/fasd/init.zsh
@@ -49,9 +49,70 @@ function fasd_cd {
   fi
 }
 
+function fcd {
+  local file="$@"
+  if [[ -d "$file" ]]; then
+    cd "$file"
+  else
+    print "$file"
+  fi
+}
+
+_fasd_generate_matches() {
+  fasd -l "$@" 2>&1 | sed '/^$/d' | sed -e "s,^$HOME,~,"
+}
+
+_fasd_zsh_completion() {
+  setopt localoptions noshwordsplit noksh_arrays noposixbuiltins nonomatch
+  local args cmd selected slug
+
+  args=(${(z)LBUFFER})
+  cmd=${args[1]}
+
+  if [[ "$cmd" != "fcd" || "$LBUFFER" =~ "^\s*fcd$" ]]; then
+    zle ${_fasd_zsh_default_completion:-expand-or-complete}
+    return
+  fi
+
+  if [[ "${#args}" -gt 1 ]]; then
+    eval "slug=${args[-1]}"
+  fi
+
+  if [[ "$(_fasd_generate_matches "$slug" | head | wc -l)" -gt 1 ]]; then
+    selected=$(_fasd_generate_matches "$slug" \
+      | FZF_DEFAULT_OPTS="--height ${FZF_TMUX_HEIGHT:-40%} --reverse \
+      --bind 'shift-tab:up,tab:down' $FZF_DEFAULT_OPTS" fzf-tmux)
+  elif [[ "$(_fasd_generate_matches "$slug" | head | wc -l)" -eq 1 ]]; then
+    selected=$(_fasd_generate_matches "$slug")
+  else
+    return
+  fi
+
+  if [[ -n "$selected" ]]; then
+    LBUFFER="$cmd $selected"
+  fi
+
+  zle redisplay
+  typeset -f zle-line-init >/dev/null && zle zle-line-init
+}
+
+_fasd_init_zsh_completion() {
+  zle -N _fasd_zsh_completion
+  bindkey '^I' _fasd_zsh_completion
+}
+
+_fasd_init_zsh_completion
+
 #
 # Aliases
 #
 
-# Changes the current working directory interactively.
-alias j='fasd_cd -i'
+alias a='fasd -a'        # any
+alias s='fasd -si'       # show / search / select
+alias d='fasd -d'        # directory
+alias f='fasd -f'        # file
+alias sd='fasd -sid'     # interactive directory selection
+alias sf='fasd -sif'     # interactive file selection
+alias z='fasd_cd -d'     # cd, same functionality as j in autojump
+alias zz='fasd_cd -d -i' # cd with interactive selection
+alias j='fasd_cd -i'     # cd with interactive selection
