01d8ec986efd848d193033b41aa13a8abb56b9e9
Jeoygin Wang <jeoygin@gmail.com>
2018-03-27 17:09:03 -0700

    Add FZF_PREVIEW_BINDING to accept user defined fzf preview binding

--- modules/fzf/git.zsh
+++ modules/fzf/git.zsh
@@ -1,3 +1,5 @@
+export FZF_PREVIEW_DEFAULT_BINDING="ctrl-j:preview-down,ctrl-k:preview-up,ctrl-f:preview-page-down,ctrl-b:preview-page-up,ctrl-space:toggle-preview,alt-enter:toggle-preview-wrap"
+
 is_in_git_repo() {
   git rev-parse HEAD > /dev/null 2>&1
 }
@@ -10,7 +12,7 @@ fzf-gf() {
   is_in_git_repo || return
   git -c color.status=always status --short |
   fzf-down -m --ansi --nth 2..,.. \
-    --bind 'ctrl-n:preview-down,ctrl-p:preview-up,ctrl-v:preview-page-down,alt-v:preview-page-up,ctrl-space:toggle-preview,alt-enter:toggle-preview-wrap' \
+    --bind "${FZF_PREVIEW_BINDING:-$FZF_PREVIEW_DEFAULT_BINDING}" \
     --preview '(git diff --color=always -- {-1} | sed 1,4d; cat {-1}) | head -'$LINES |
   cut -c4- | sed 's/.* -> //'
 }
@@ -19,7 +21,7 @@ fzf-gb() {
   is_in_git_repo || return
   git branch -a --color=always | grep -v '/HEAD\s' | sort |
   fzf-down --ansi --multi --tac --preview-window right:70% \
-    --bind 'ctrl-n:preview-down,ctrl-p:preview-up,ctrl-v:preview-page-down,alt-v:preview-page-up,ctrl-space:toggle-preview,alt-enter:toggle-preview-wrap' \
+    --bind "${FZF_PREVIEW_BINDING:-$FZF_PREVIEW_DEFAULT_BINDING}" \
     --preview 'git log --oneline --graph --date=short --pretty="format:%C(auto)%cd %h%d %s" $(sed s/^..// <<< {} | cut -d" " -f1) | head -'$LINES |
   sed 's/^..//' | cut -d' ' -f1 |
   sed 's#^remotes/##'
@@ -29,7 +31,7 @@ fzf-gt() {
   is_in_git_repo || return
   git tag --sort -version:refname |
   fzf-down --multi --preview-window right:70% \
-    --bind 'ctrl-n:preview-down,ctrl-p:preview-up,ctrl-v:preview-page-down,alt-v:preview-page-up,ctrl-space:toggle-preview,alt-enter:toggle-preview-wrap' \
+    --bind "${FZF_PREVIEW_BINDING:-$FZF_PREVIEW_DEFAULT_BINDING}" \
     --preview 'git show --color=always {} | head -'$LINES
 }
 
@@ -37,7 +39,7 @@ fzf-gh() {
   is_in_git_repo || return
   git log --date=short --format="%C(green)%C(bold)%cd %C(auto)%h%d %s (%an)" --graph --color=always |
   fzf-down --ansi --no-sort --reverse --multi \
-    --bind 'ctrl-s:toggle-sort,ctrl-n:preview-down,ctrl-p:preview-up,ctrl-v:preview-page-down,alt-v:preview-page-up,ctrl-space:toggle-preview,alt-enter:toggle-preview-wrap' \
+    --bind "ctrl-s:toggle-sort,${FZF_PREVIEW_BINDING:-$FZF_PREVIEW_DEFAULT_BINDING}" \
     --header 'Press CTRL-S to toggle sort' \
     --preview 'grep -o "[a-f0-9]\{7,\}" <<< {} | xargs git show --color=always | head -'$LINES |
   grep -o "[a-f0-9]\{7,\}"
@@ -47,7 +49,7 @@ fzf-gr() {
   is_in_git_repo || return
   git remote -v | awk '{print $1 "\t" $2}' | uniq |
   fzf-down --tac \
-    --bind 'ctrl-n:preview-down,ctrl-p:preview-up,ctrl-v:preview-page-down,alt-v:preview-page-up,ctrl-space:toggle-preview,alt-enter:toggle-preview-wrap' \
+    --bind "${FZF_PREVIEW_BINDING:-$FZF_PREVIEW_DEFAULT_BINDING}" \
     --preview 'git log --oneline --graph --date=short --pretty="format:%C(auto)%cd %h%d %s" {1} | head -200' |
   cut -d$'\t' -f1
 }
